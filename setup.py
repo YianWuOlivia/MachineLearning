c1a3c6966a58330824ccaefbeeb98ef810ff5af2
YianWuOlivia <109147903+YianWuOlivia@users.noreply.github.com>
2022-07-12 07:25:57 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+  name = "MachineLearning",
+  author = "Yian Wu",
+  author_email = "yian060223@gmail.com"
+  description == "This is my math library."
+  packages = ["MachineLearning]
+)
